6044a125a62eec7c059d82b4271c1c8331ca7312
rendy.prawira <rendy.prawira@stockbit.com>
2023-04-17 08:41:55 +0700

    feat: home pages wording

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,16 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div>Home</div>
+    <div className="mx-4">
+      <div>
+        <h1 className="font-semibold text-2xl">
+          Hello Everyone,
+        </h1>
+        <h2 className="text-md">
+          I'm <b className="text-md">Rendy Artha Prawira</b>{' '}--{' '}Web Engineer
+        </h2>
+      </div>
+    </div>
   );
 };
 
