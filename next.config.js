c89348bd94728aa66c89ef85a180af531833a7a5
rendy.prawira <rendy.prawira@stockbit.com>
2023-05-15 15:29:26 +0700

    feat: add content home section

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     domains: [
       "s3.us-west-2.amazonaws.com",
       "https://res.cloudinary.com/"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,32 @@
 import type { NextPage } from "next";
+import Image from "next/image";
+import GifSectionHome from "~/public/icons/section-home.gif";
 
 const Home: NextPage = () => {
   return (
-    <div className="mx-4">
-      <div>
-        <h1 className="font-semibold text-2xl">
-          Hello Everyone,
-        </h1>
-        <h2 className="text-md">
-          I'm <b className="text-md">Rendy Artha Prawira</b>{' '}--{' '}Web Engineer
-        </h2>
+    <div className="mx-4 my-8">
+      <div className="flex flex-row justify-between items-center">
+        <div>
+          <h1 className="font-semibold text-2xl">
+            Hello Everyone,
+          </h1>
+          <h2 className="text-md">
+            I'm <b className="text-md">Rendy Artha Prawira</b>{' '}--{' '}Web Engineer
+          </h2>
+          <p>
+            Gathering the results of learning as my journey, which I gathered into one platform.
+            Hope it is useful for you all. If you need my services, please reach me out.
+          </p>
+          <p>
+            I usually use this stack
+          </p>
+        </div>
+        <Image
+          src={GifSectionHome}
+          width={800}
+          height={500}
+          unoptimized={true}
+        />
       </div>
     </div>
   );
