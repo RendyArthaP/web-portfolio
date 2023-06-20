1c2b836307600aba91151d389f99d2d4afab4334
rendy.prawira <rendy.prawira@stockbit.com>
2023-06-20 21:33:43 +0700

    fix: images

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    disableStaticImages: true,
     unoptimized: true,
     domains: ["s3.us-west-2.amazonaws.com", "https://res.cloudinary.com/"],
   },
